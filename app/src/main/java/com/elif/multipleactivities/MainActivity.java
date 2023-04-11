dcd6465beec4224997aa96331591e6ed45fddf66
elif <eelifaakay@gmail.com>
2023-04-11 17:01:42 +0300

    a transfer of knowledge between activities

--- app/src/main/java/com/elif/multipleactivities/MainActivity.java
+++ app/src/main/java/com/elif/multipleactivities/MainActivity.java
@@ -5,18 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    String personName;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.editText);
+
+        personName="";
+
     }
 
     public void changeActivity(View view)
     {
+        //kullanıcının girdiği veriyi aldık eşitledik
+        personName = editText.getText().toString();
+
         Intent intent =new Intent(MainActivity.this,MainActivity2.class);
+
+        intent.putExtra("username",personName);
         startActivity(intent);
+
+
+
     }
 }
--- app/src/main/java/com/elif/multipleactivities/MainActivity2.java
+++ app/src/main/java/com/elif/multipleactivities/MainActivity2.java
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        textView=findViewById(R.id.PersonName);
+        Intent intent = getIntent();
+        String personName =intent.getStringExtra("username");
+        textView.setText(personName);
+
     }
 
     public void changeActivity2(View view)
     {
         Intent intent =new Intent(MainActivity2.this,MainActivity.class);
+
         startActivity(intent);
+
+
     }
 }
